08c07e5eea69e451ac3dac84b62314f420b76b67
finn <bishoe01@gmail.com>
2025-08-24 09:32:15 +0900

    fix: ReportEditorView Hi-Fi 반영

--- Seniority/Seniority/Sources/Models/Domain/SavedReport.swift
+++ Seniority/Seniority/Sources/Models/Domain/SavedReport.swift
@@ -3,20 +3,12 @@ import Foundation
 // Appstorage KeyName : SavedReport
 struct SavedReport: Codable {
     var responseText: String
-    
     var recipient: String
-    
-    var sendTimeString: String
-    
-    var savedAt: String
-    
-    init(responseText: String, recipient: String, sendTimeString: String) {
+    var savedAt: Date
+
+    init(responseText: String, recipient: String) {
         self.responseText = responseText
         self.recipient = recipient
-        self.sendTimeString = sendTimeString
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy.MM.dd HH:mm"
-        self.savedAt = formatter.string(from: Date())
+        self.savedAt = Date()
     }
 }
